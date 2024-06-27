20f3734fbb2b6fe55722202e53771cabd872a3d3
Raphael Ferreira de Moraes <raphael.ferreira@litoraltextil.com.br>
2024-06-27 17:11:26 -0300

    ajusta imports

--- android/src/main/java/com/mycompany/plugins/example/InstallerPlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/InstallerPlugin.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.util.Log;
+
+import androidx.core.content.FileProvider;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
