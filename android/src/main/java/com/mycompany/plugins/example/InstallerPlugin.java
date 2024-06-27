78237551065453f69aaca9e638e80ef9c218e4cc
Raphael Ferreira de Moraes <raphael.ferreira@litoraltextil.com.br>
2024-06-27 17:42:36 -0300

    Volta codigo

--- android/src/main/java/com/mycompany/plugins/example/InstallerPlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/InstallerPlugin.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.util.Log;
 
 import androidx.core.content.FileProvider;
 
@@ -37,20 +36,13 @@ public class InstallerPlugin extends Plugin {
             return;
         }
 
-        try {
-            Uri apkUri = getUriFromFile(context, apkFile);
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(getUriFromFile(context, apkFile), "application/vnd.android.package-archive");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-            Log.d("InstallerPlugin", "Starting APK installation intent.");
-            context.startActivity(intent);
-            call.resolve();
-        } catch (Exception e) {
-            Log.e("InstallerPlugin", "Error installing APK", e);
-            call.reject("Error installing APK: " + e.getMessage());
-        }
+        context.startActivity(intent);
+        call.resolve();
     }
 
     private Uri getUriFromFile(Context context, File file) {
